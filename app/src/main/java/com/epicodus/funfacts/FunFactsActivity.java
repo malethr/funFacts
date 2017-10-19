60226ff631c84a843d2104b260c179618bbd2759
Maria Thomas <malethr@gmail.com>
2017-10-19 10:18:27 -0700

    create initial funfact app

---
+++ app/src/main/java/com/epicodus/funfacts/FunFactsActivity.java
@@ -0,0 +1,47 @@
+package com.epicodus.funfacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class FunFactsActivity extends AppCompatActivity {
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
+    private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
+    private TextView mFactTextView;
+    private Button mShowFactButton;
+    private RelativeLayout mRelativeLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        // Assign views to corresponding variables
+        mFactTextView = (TextView) findViewById(R.id.factTextView);
+        mShowFactButton = (Button) findViewById(R.id.showFactButton);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = mFactBook.getFact();
+                mFactTextView.setText(fact);
+
+                int color = mColorWheel.getColor();
+                mRelativeLayout.setBackgroundColor(color);
+                mShowFactButton.setTextColor(color);
+
+            }
+        };
+
+        mShowFactButton.setOnClickListener(listener);
+        // Toast.makeText(this, "Yay! Our Activity is working!", Toast.LENGTH_SHORT).show();
+        Log.d(TAG,"Activity Created!");
+
+    }
+}
